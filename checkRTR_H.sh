5f77f7c52fa6427fce286b01b62ec9e1f9f03ace
CaptainCommandos <129690577+CaptainCommandos@users.noreply.github.com>
2026-05-13 14:26:01 +0300

    1

---
+++ checkRTR_H.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Имя устройства:"
+hostname
+echo
+
+# Проверяем интернет через ping до DNS Google
+echo "Проверка интернета через ping до ya.ru"
+echo
+
+ping -c 2 ya.ru
+
+echo
+echo "Сетевые интерфейсы и IP-адреса:"
+
+# Выводим IP-адреса с маской подсети
+ip -o -4 addr show | awk '{print "Интерфейс: " $2 "\nIP-адрес/маска: " $4 "\n"}'
