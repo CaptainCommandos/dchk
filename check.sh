e361c547ccfe64221aa3fac92b27652b38a4d69b
CaptainCommandos <129690577+CaptainCommandos@users.noreply.github.com>
2026-05-14 11:12:31 +0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -245,7 +245,7 @@ else
     else
         echo "systemctl не найден. Невозможно проверить службу FRR."
     fi
- pause_script
+    pause_script
     echo
     echo "=== Проверка демона ospfd ==="
     echo
@@ -265,7 +265,7 @@ else
         echo "Проверка OSPF завершена."
         return 2>/dev/null || true
     fi
-
+    pause_script
     echo
     echo "=== Конфигурация OSPF ==="
     echo
@@ -281,7 +281,7 @@ else
     else
         echo "Конфигурация router ospf не найдена."
     fi
-pause_script
+    pause_script
     echo
     echo "=== Интерфейсы OSPF ==="
     echo
@@ -293,7 +293,7 @@ pause_script
     else
         echo "Интерфейсы OSPF не найдены или OSPF не настроен."
     fi
-pause_script
+    pause_script
     echo
     echo "=== Соседи OSPF ==="
     echo
@@ -305,7 +305,7 @@ pause_script
     else
         echo "OSPF-соседи не найдены."
     fi
-pause_script
+    pause_script
     echo
     echo "=== Маршруты OSPF ==="
     echo
